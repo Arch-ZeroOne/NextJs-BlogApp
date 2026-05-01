e30d333e6a833c66c1e7a220ed8b292b83e893df
Arch-ZeroOne <windyl1.monton.lvhs@gmail.com>
2026-05-01 23:21:16 +0800

    Changed the index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,83 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useEffect, useState } from "react";
+import { Post, index } from "@/services/posts";
+const Main = () => {
+  const [posts, setPosts] = useState<Post[]>();
+  const [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    loadPosts();
+  }, []);
+
+  useEffect(() => {
+    if (searchInput) {
+      const filtered = posts?.filter(
+        (data) =>
+          data.author.toLowerCase().includes(searchInput.toLowerCase()) ||
+          data.description.toLowerCase().includes(searchInput.toLowerCase()) ||
+          data.title.toLowerCase().includes(searchInput.toLowerCase()),
+      );
+
+      console.log(filtered);
+      setPosts(filtered);
+    } else {
+      loadPosts();
+    }
+  }, [searchInput]);
+  const loadPosts = async () => {
+    const posts = await index();
+
+    setPosts(posts);
+  };
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <main className="flex flex-col w-full gap-4 p-3">
+      {/* Search Bar */}
+      <label className="input w-[950] self-center">
+        <svg
+          className="h-[1em] opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </g>
+        </svg>
+        <input
+          value={searchInput}
+          type="search"
+          required
+          placeholder="Search"
+          onChange={(e) => setSearchInput(e.target.value)}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </label>
+      <section className="grid grid-cols-3">
+        {posts &&
+          posts.map((data: Post) => (
+            <div className="card bg-base-100 w-96 shadow-sm">
+              <figure>
+                <img src={data.image_url} alt="Shoes" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{data.title}</h2>
+                <p>{data.description}</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Read More</button>
+                </div>
+              </div>
+            </div>
+          ))}
+      </section>
+    </main>
   );
-}
+};
+
+export default Main;
